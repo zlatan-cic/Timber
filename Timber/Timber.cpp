b594996daef1ed7bdeeaa73418b9a84318a417a9
zlatan-cic <smajicz999@gmail.com>
2025-05-03 10:27:50 +0200

    Implement player input handling for left and right movement

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cmath>
-
+#include <SFML/Audio.hpp>
 
 using namespace sf;
 using namespace std;
@@ -189,6 +189,50 @@ int main()
 	updateBranches(4);
 	updateBranches(5);
 
+	// Prepare the player
+	Texture texturePlayer;
+	texturePlayer.loadFromFile("graphics/player.png");
+	Sprite spritePlayer;
+	spritePlayer.setTexture(texturePlayer);
+	spritePlayer.setPosition(580, 720);
+
+	//The player starts on the left
+	side playerSide = side::LEFT;
+
+	// Prepare the gravestone
+	Texture textureRIP;
+	textureRIP.loadFromFile("graphics/rip.png");
+	Sprite spriteRIP;
+	spriteRIP.setTexture(textureRIP);
+	spriteRIP.setPosition(600, 860);
+
+	//Prepare the axe
+	Texture textureAxe;
+	textureAxe.loadFromFile("graphics/axe.png");
+	Sprite spriteAxe;
+	spriteAxe.setTexture(textureAxe);
+	spriteAxe.setPosition(700, 830);
+
+	//Line the axe up with the tree
+	const float AXE_POSITION_LEFT = 700;
+	const float AXE_POSITION_RIGHT = 1075;
+
+	//Prepare the flying log
+	Texture textureLog;
+	textureLog.loadFromFile("graphics/log.png");
+	Sprite spriteLog;
+	spriteLog.setTexture(textureLog);
+	spriteLog.setPosition(810, 720);
+
+	// Some other useful log related variables
+	bool logActive = false;
+	float logSpeedX = 1000;
+	float logSpeedY = -1500;
+
+	//Control the player input
+	bool acceptInput = false;
+
+
 	while (window.isOpen())
 	{
 		/*
@@ -196,6 +240,20 @@ int main()
 			Handle the players input
 			****************************************
 		*/
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyReleased && !paused)
+			{
+				// Listen for key presses again
+				acceptInput = true;
+
+				//hide the axe 
+				spriteAxe.setPosition(2000, spriteAxe.getPosition().y);
+			}
+		}
+
 
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
@@ -208,7 +266,83 @@ int main()
 			// Reset the time and the score
 			score = 0;
 			timeRemaining = 6;
+
+			//Make all the branches disappear
+			for (int i = 1; i < NUM_BRANCHES; i++)
+			{
+				branchPositions[i] = side::NONE;
+			}
+
+			//Make sure the grave stone is hidden
+			spriteRIP.setPosition(675, 2000);
+			/// Move the player into position
+			spritePlayer.setPosition(580, 720);
+
+			acceptInput = true;
 		}
+
+		// Wrap the player controls to
+		// Make sure we are accepting inpu
+		if (acceptInput)
+		{
+			// First handle pressing the right cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Right))
+			{
+				//Make sure the player is on the right
+				playerSide = side::RIGHT;
+
+				score++;
+
+				//Add to the amount of time remaining
+				timeRemaining += (2 / score) + .15;
+
+				spriteAxe.setPosition(AXE_POSITION_RIGHT,
+					spriteAxe.getPosition().y);
+
+				spritePlayer.setPosition(1200, 720);
+
+				//Update the branch
+				updateBranches(score);
+
+				//set the log flying to the left
+				spriteLog.setPosition(810, 720);
+				logSpeedX = -5000;
+				logActive = true;
+
+				acceptInput = false;
+			}
+
+
+			// Handle the left cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Left))
+			{
+				// Make sure the player is on the left
+				playerSide = side::LEFT;
+
+				score++;
+
+				// Add to the amount of time remaining
+				timeRemaining += (2 / score) + .15;
+
+				spriteAxe.setPosition(AXE_POSITION_LEFT,
+					spriteAxe.getPosition().y);
+
+				spritePlayer.setPosition(580, 720);
+
+				// update the branches
+				updateBranches(score);
+
+				// set the log flying
+				spriteLog.setPosition(810, 720);
+				logSpeedX = 5000;
+				logActive = true;
+
+
+				acceptInput = false;
+			}
+
+		}
+
 
 		/*
 		******** Update the scene
@@ -356,6 +490,49 @@ int main()
 					cloud3Active = false;
 				}
 			}
+			// Handle a flying log
+			if (logActive)
+			{
+				spriteLog.setPosition(
+					spriteLog.getPosition().x +
+					(logSpeedX * dt.asSeconds()),
+					spriteLog.getPosition().y +
+					(logSpeedY * dt.asSeconds()));
+
+				//Has the log reach right angle
+				if (spriteLog.getPosition().x < -100 ||
+					spriteLog.getPosition().y > 2000)
+				{
+					//Set ti up ready to ber a whole new log next frame
+					logActive = false;
+					spriteLog.setPosition(810, 720);
+				}
+			}
+			if (branchPositions[5] == playerSide)
+			{
+				//death
+				paused = true;
+				acceptInput = false;
+
+				//Draw the grave stone
+				spriteRIP.setPosition(525, 760);
+
+				//hide the player
+				spritePlayer.setPosition(2000, 660);
+
+				// Change the text of the message
+				messageText.setString("SQUISHED!!");
+
+				//Cneter text on the scren
+				FloatRect text = messageText.getLocalBounds();
+
+				messageText.setOrigin(textRect.left +
+					textRect.width / 2.0f,
+					textRect.top + textRect.height / 2.0f);
+
+				messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+			}
 		}
 
 		
@@ -391,6 +568,8 @@ int main()
 			}
 		}
 
+		
+
 		// Clear everything from the last frame window.clear();
 		window.clear();
 
@@ -417,6 +596,16 @@ int main()
 		}
 		// Draw the tree
 		window.draw(spriteTree);
+
+		// Draw the player
+		window.draw(spritePlayer);
+		// Draw the axe
+		window.draw(spriteAxe);
+		// Draw the flying log
+		window.draw(spriteLog);
+		// Draw the gravestone
+		window.draw(spriteRIP);
+
 		// Draw the Bee
 		window.draw(spriteBee);
 
